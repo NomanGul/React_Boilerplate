64244658ffcc4b2cfddfa000301847ab1e83a368
Noman Gul <nomangul2001@gmail.com>
2018-08-07 23:42:51 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <h1 className="animated wobble delay-5s">Hello Pakistan!</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
